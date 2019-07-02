10312dd6903fc8f4023ddca0f3760c10f3202408
k9isawesome <jzhou381@gatech.edu>
2019-07-01 20:35:52 -0400

    Added movement when press 'a' or 'd' for trainer position

--- test.py
+++ test.py
@@ -2,15 +2,22 @@
 import pygame
 import random
 
+# gravity value
+gravity = 20
+
+# move_speed value
+move_speed = 1
 
 def load_images():
     image_dict = {}
-    image_dict['grassTop'] = pygame.image.load("assets/grass/GrassTop.png").convert()
-    image_dict['grassTopRock'] = pygame.image.load("assets/grass/GrassTopRock.png").convert()
-    image_dict['trainer'] = pygame.image.load("assets/trainer.png").convert()
+    image_dict['grass_top'] = pygame.image.load("assets/grass/GrassTop.png").convert_alpha()
+    image_dict['grass_top_rock'] = pygame.image.load("assets/grass/GrassTopRock.png").convert_alpha()
+    image_dict['trainer'] = pygame.image.load("assets/trainer.png").convert_alpha()
+    image_dict['bg'] = pygame.image.load("assets/bg.png").convert_alpha()
     return image_dict
 
-def draw_background(screen, image_dict):
+
+def draw_foreground(screen, image_dict):
 
     grass_top = image_dict['grass_top']
     grass_top_rock = image_dict['grass_top_rock']
@@ -35,26 +42,32 @@ def main():
     # create a surface on screen that has the size of 240 x 180
     screen = pygame.display.set_mode((240, 180))
 
+    # set up clock for stable framerate
+    clock = pygame.time.Clock()
+
     # load images
-    imageDict = load_images()
+    image_dict = load_images()
 
     # draw background
-    draw_background(screen, imageDict)
+    screen.blit(image_dict['bg'], (0, 0))
 
-    # draw trainer
+    # draw foreground
+    draw_foreground(screen, image_dict)
+
+    # initialize trainer position
     trainer_pos_x = 0
     trainer_pos_y = 0
-    screen.blit(imageDict['trainer'], (trainer_pos_x, trainer_pos_y))
-    pygame.display.flip()
-
-    gravity = 20
-    move_speed = 10
 
     # define a variable to control the main loop
     running = True
 
     # main loop
     while running:
+
+        # erase trainer position
+        trainer_rect = pygame.Rect(trainer_pos_x, trainer_pos_y, 32, 32)
+        screen.blit(image_dict['bg'], trainer_rect, trainer_rect)
+
         # event handling, gets all event from the event queue
         for event in pygame.event.get():
             # only do something if the event is of type QUIT
@@ -62,13 +75,24 @@ def main():
                 # change the value to False, to exit the main loop
                 running = False
 
-        # draw/update player position
+        keys = pygame.key.get_pressed()
+
+        # update trainer position based on key press
+        if keys[pygame.K_d]:
+            trainer_pos_x += move_speed
+        if keys[pygame.K_a]:
+            trainer_pos_x -= move_speed
+
+        # apply gravity to trainer position
         trainer_pos_y += gravity
         if trainer_pos_y > 132:
             trainer_pos_y = 132
-        screen.blit(imageDict['trainer'], (trainer_pos_x, trainer_pos_y))
+
+        # draw new trainer position
+        screen.blit(image_dict['trainer'], (trainer_pos_x, trainer_pos_y))
 
         pygame.display.flip()
+        clock.tick(30)
 
     pygame.quit()
 
